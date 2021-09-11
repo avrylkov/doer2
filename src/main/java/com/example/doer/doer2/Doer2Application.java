99c6b33ca5f919f1e1cc43c05be7d32cb618a50b
avrylkov <arylkov70@mail.ru>
2021-09-11 17:35:32 +0300

    1.2

--- src/main/java/com/example/doer/doer2/Doer2Application.java
+++ src/main/java/com/example/doer/doer2/Doer2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-context.xml")
 public class Doer2Application {
 	public static void main(String[] args) {
 
